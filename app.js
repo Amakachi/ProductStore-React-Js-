163776b1a166a024ed852d4e86942c3fd0f50ae7
EmmanuelAbajo <emmanueltemiloluwa97@gmail.com>
2020-01-30 17:38:17 +0100

    enable cors functionality added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const { mongoose } = require('./db/mongoose');
 const { Product } = require('./entities/product-detail');
@@ -8,6 +9,7 @@ const ProductRouter = require('./routes/product-routes')(Product);
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors()) // enables CORS for all routes
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/uploads',express.static('uploads'))
