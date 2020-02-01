44fcb9f3a6a71e90ed3d1f569e7e46740542839f
Chiamaka Okenwa <chiamakaokenwa@gmail.com>
2020-02-01 02:00:08 +0100

    New Changes

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
+const { mongoose } = require('./db/mongoose');
+mongoose.connect('mongodb://admin:daniella1@ds149998.mlab.com:49998/heroku_hxfjqrf6', {useNewUrlParser: true});
+mongoose.Promise = global.Promise;
 
-// const { mongoose } = require('./db/mongoose');
+
 const { Product } = require('./entities/product-detail');
 const ProductRouter = require('./routes/product-routes')(Product);
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+
 
 app.use(cors()) // enables CORS for all routes
 app.use(bodyParser.json());
